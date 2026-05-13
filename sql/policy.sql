526385250d689b71ee7a00f5f5d7acfad9d1963e
CrisMendezQA <crismendezfotografia@gmail.com>
2026-05-13 21:49:44 +0200

    update policy

--- sql/policy.sql
+++ sql/policy.sql
@@ -3,3 +3,17 @@ ON formcrismendez
 FOR INSERT
 TO anon
 WITH CHECK (true);
+
+-- asegurar RLS activo
+ALTER TABLE formcrismendez ENABLE ROW LEVEL SECURITY;
+
+-- permisos base
+GRANT SELECT ON TABLE formcrismendez TO anon;
+GRANT INSERT ON TABLE formcrismendez TO anon;
+
+-- policies
+CREATE POLICY "Allow select anon"
+ON formcrismendez
+FOR SELECT
+TO anon
+USING (true);
